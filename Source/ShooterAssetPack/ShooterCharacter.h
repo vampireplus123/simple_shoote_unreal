f2556e3713180a6760c4c64ea648f56e74e4a452
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-03-26 22:37:09 +0700

    update

---
+++ Source/ShooterAssetPack/ShooterCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class SHOOTERASSETPACK_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+private:
+	void MoveForward(float AxisValue);
+	void LookUp(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookRight(float AxisValue);
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
+
+
